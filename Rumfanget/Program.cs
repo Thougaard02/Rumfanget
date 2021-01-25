8971e2e3bf5b0aade80ed1d89579b789698ee924
Rasmus <Rasmus@vinperlen.dk>
2021-01-25 10:26:57 +0100

    Add project files.

---
+++ Rumfanget/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rumfanget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Rumfang af kasse");
+            Console.WriteLine("Bestem højden");
+            double h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bestem bredden");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Bestem længden");
+            double l = double.Parse(Console.ReadLine());
+
+            string v = $"Rumfanget af kassen = {h * b * l}";
+
+            Console.WriteLine(v);
+        }
+    }
+}
